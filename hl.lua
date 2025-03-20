5c758cbf193e1fe4c084f6432ab2d7249fa06f0f
mg104 <madhurgupta104@gmail.com>
2025-03-20 10:10:27 +0530

    Reformat the comments a bit

--- hl.lua
+++ hl.lua
@@ -1,9 +1,9 @@
--- Lua script to highlight strings in an Nvim buffer using perl instead of vim regex tool (/)
+-- Lua script to highlight strings in an Nvim buffer using perl instead of vim regex search tool (/)
 
 local buf_text_table = vim.api.nvim_buf_get_lines(0, 0, -1, true)    -- Get the perl query file
 local buf_text_single_string = table.concat(buf_text_table, "\n")    -- Read the lines from the buffer into a lua tableb
 local perl_script_location = './search_highlight.pl'                 -- Store the location of the perl file into a string
-local perl_regex = 'local'                                           -- Capture the perl regex query
+local perl_regex = 'local'                                           -- Capture the perl regex query (note that I'm working on adding functionality to be able to provide this query as a dynamic argument, rather than hard coding it here)
 
 local perl_command = string.format(                                  -- Create the perl command to be called by lua & run the perl command on the text
    "perl %s %s",
@@ -13,8 +13,7 @@ local perl_command = string.format(                                  -- Create t
 
 local perl_command_output_table = vim.fn.systemlist(perl_command, buf_text_single_string) -- Output from the perl script containing multiple lines
                                                                                           -- with each line being of the form: lineno startcolno endcolno
-local match_table = {} -- Lua table that will be used later to keep record of the current cursor position and next/prev cursor position (for the functionality
-                       -- to scroll forward/backward through the highlighted regex-matches using 'n' and 'N')
+local match_table = {} -- Lua table that will be used later to keep record of the current cursor position and next/prev cursor position (for the functionality to scroll forward/backward through the highlighted regex-matches using 'n' and 'N')
 
 local perl_namespace_id = vim.api.nvim_create_namespace('perl_highlight')  -- Lua namespace that contains the record of all the highlights made using this script
 vim.api.nvim_buf_clear_namespace(0, perl_namespace_id, 0, -1)              -- Clear the previous highlights that were made using this lua + perl script
@@ -43,7 +42,6 @@ match_table_rev = {unpack(match_table)}                                         
                                                                                                          -- (used for backward scroll)
 table.sort(match_table_rev, function(a, b) return a[1] > b[1] or ((a[1] == b[1]) and (a[2] > b[2])) end)
 
-local match_row_max, match_col_max = match_table[#match_table][1], match_table[#match_table][2] -- Coordinates of the last match in the nvim buffer (for wrapping around the scroll when using 'n')
 local match_row_min, match_col_min = match_table[1][1], match_table[1][2]                       -- Coordinates of the first match in the nvim buffer (for wrapping around the scroll when pressing 'N')
 
 function goto_nxt_perl_match(match_table)                            -- Function to make the cursor go to the next regex match (useful for scrolling between matches)
@@ -58,8 +56,7 @@ function goto_nxt_perl_match(match_table)                            -- Function
       )
    else 
       for i, v in ipairs(match_table) do
-         local match_row, match_col = v[1], v[2]                     -- Loop through each match's coordinates and stop at a point on the first match satisfying the if condition below
-                                                                     -- to get the next and nearest match relative to the cursor position
+         local match_row, match_col = v[1], v[2]                     -- Loop through each match's coordinates and stop at a point on the first match satisfying the if condition below to get the next and nearest match relative to the cursor position
          if (
             (match_row > curs_row) or                                -- If the cursor position is not past the position of the last match in the nvim buffer, go to the next match
             ((match_row == curs_row) and (match_col > curs_col))
