b77765d5873954dfecfe700e4b7303a74093efba
mg104 <madhurgupta104@gmail.com>
2025-03-20 10:07:04 +0530

    Initial commit. Add perl regex matching script and lua script to enable the perl script to interact with nvim.

---
+++ search_highlight.pl
@@ -0,0 +1,15 @@
+#!/usr/bin/perl
+use strict;
+use warnings;
+
+# Read the regex pattern from command-line arguments
+my $query = shift or die "Usage: $0 <regex-pattern>\n";
+
+my $i = 1;
+while (<>) {
+   while ($_ =~ /$query/g) {
+      print "$i $-[0] $+[0]\n";
+   }
+   $i++;
+}
+
