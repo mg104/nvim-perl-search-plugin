848f2f8434824022815862fe2be8a035df147899
mg104 <madhurgupta104@gmail.com>
2025-03-20 12:40:13 +0530

    Modify search_highlight.pl start line numbering of the nvim buffer, from 0 (instead of 1 earlier)

--- search_highlight.pl
+++ search_highlight.pl
@@ -5,7 +5,7 @@ use warnings;
 # Read the regex pattern from command-line arguments
 my $query = shift or die "Usage: $0 <regex-pattern>\n";
 
-my $i = 1;
+my $i = 0;
 while (<>) {
    while ($_ =~ /$query/g) {
       print "$i $-[0] $+[0]\n";
